fa81d2fd7d1325682fcc0bebe9156221692d7241
Liapo <42915611+Liapo@users.noreply.github.com>
2019-04-05 16:10:28 +0300

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[]args){
+        Scanner in=new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n=in.nextInt();
+        int ar[]=new int[n];
+        for(int i=0;i<n;i++){
+                System.out.print("a" + (i + 1) + "=");
+                ar[i]=in.nextInt();
+        }
+        for(int i=0;i<n;i++)
+                System.out.print(ar[i]+" ");
+
+        System.out.println();
+
+        for(int i=0;i<n;i++)
+            System.out.println(ar[i]+" ");
+
+
+
+    }
+}
